89a4163697325854113f23ac9ec7829f638fc433
AugustoAleGon <aagg83556@hotmail.com>
2021-10-03 16:38:50 -0500

    Set up Dummy Tabs

--- Challenge/navigation/AppContainer.tsx
+++ Challenge/navigation/AppContainer.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import MainTabs from './MainTabs';
 
 const RootStack = createNativeStackNavigator();
 
 const AppContainer = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <RootStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{headerShown: false}}>
-        <RootStack.Screen name="Home" component={MainTabs} />
-      </RootStack.Navigator>
-    </SafeAreaView>
+    <RootStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
+      <RootStack.Screen name="Home" component={MainTabs} />
+    </RootStack.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
-});
 
 export default AppContainer;
--- Challenge/navigation/MainTabs.tsx
+++ Challenge/navigation/MainTabs.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeView from '../screens/HomeView';
+import VaultView from '../screens/VaultView';
+import SearchView from '../screens/SearchView';
+import AlertView from '../screens/AlertView';
+import ProfileView from '../screens/ProfileView';
+
+const Tab = createBottomTabNavigator();
 
 const MainTabs = () => {
   return (
-    <View>
-      <Text>Hello from Main Tabs</Text>
-    </View>
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name="Home" component={HomeView} />
+      <Tab.Screen name="Vault" component={VaultView} />
+      <Tab.Screen name="Search" component={SearchView} />
+      <Tab.Screen name="Alert" component={AlertView} />
+      <Tab.Screen name="Profile" component={ProfileView} />
+    </Tab.Navigator>
   );
 };
 
